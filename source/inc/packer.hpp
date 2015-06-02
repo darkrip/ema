d15781190b78edf4a650c52eb262da89e15f7de8
darkrip <dark.rip@gmail.com>
2015-06-02 18:56:31 +0300

    1

--- source/inc/packer.hpp
+++ source/inc/packer.hpp
@@ -9,9 +9,62 @@ namespace pack
 typedef std::iowstream PackDataStream;
 typedef std::wstring FileName;
 
+
+class VariablesList
+{
+};
+
+
+
+class Command
+{
+};
+
+
+
 class Packer
 {
 public:
+	Packer(const std::wstring& packerName);
+
+	void addExtension(const FileName&);
+	
+	
+	
+	
+private:
+	typedef std::set<FileName> ExtensionList;
+	typedef std::list<PackID>  PackerIdList;
+		
+		
+	String		  m_name;	
+	String		  m_description;
+	ExtensionList m_extensionList;
+	PackerIdList  m_ids;
+	
+	ErrorLevel m_errorLevel;
+	bool 	   m_batchUnpack;
+	bool	   m_skipEmpty;
+	bool 	   m_skipDirsInFileList;
+	bool 	   m_unixPath;
+	
+//List section	
+	Command m_listCommand;
+	String  m_startListParsing;
+	String  m_endListParsing;
+	String  m_ignoreStrings;
+	ListFormat m_listFormat;
+//Extract section
+	Command m_extractCommand;
+	Command m_extractWithoutPathCommand;
+	
+	Command m_testCommand;
+	Command m_deleteCommand;
+	Command m_addCommand;
+	Command m_moveCommand;
+	
+	
+
 
 };
 
